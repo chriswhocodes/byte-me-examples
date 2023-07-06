fec36d5cbda6e9ccd262c65947f29554d0d77318
Chris Newland <chris@chrisnewland.com>
2023-07-06 09:13:29 +0100

    added JEP to DESC

--- src/main/java/StringTemplate.java
+++ src/main/java/StringTemplate.java
@@ -1,11 +1,11 @@
 //SINCE:21
-//DESC:Demonstrates StringTemplate feature
+//DESC:Demonstrates StringTemplate feature from  <a href="https://openjdk.org/jeps/430">JEP 430</a>
 public class StringTemplate
 {
     public static void main(String[] args)
     {
         String name = "Chris";
-        String namedMessage = STR. "My name is \\{name}";
+        String namedMessage = STR."My name is \\{name}";
         System.out.println(namedMessage);
     }
 }
