d65ea22ab789c17096b3400796f04227f42b9341
Chris Newland <chris@chrisnewland.com>
2023-07-07 06:56:50 +0100

    fixed StringTemplate example

--- src/main/java/StringTemplate.java
+++ src/main/java/StringTemplate.java
@@ -1,3 +1,5 @@
+import static java.lang.StringTemplate.STR;
+
 //SINCE:21
 //DESC:Demonstrates StringTemplate feature from  <a href="https://openjdk.org/jeps/430">JEP 430</a>
 public class StringTemplate
@@ -5,7 +7,13 @@ public class StringTemplate
     public static void main(String[] args)
     {
         String name = "Chris";
-        String namedMessage = STR."My name is \\{name}";
+        String namedMessage = STR."My name is \{name}";
         System.out.println(namedMessage);
+
+        for (int i = 0; i < 10; i++)
+        {
+            String formatted = STR."I counted to \{i}";
+            System.out.println(formatted);
+        }
     }
 }
