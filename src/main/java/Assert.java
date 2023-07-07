2e0fac74feb78a7b76896b36b9f14a86dbaaae06
Chris Newland <chris@chrisnewland.com>
2023-07-07 10:17:49 +0100

    assert example

---
+++ src/main/java/Assert.java
@@ -0,0 +1,10 @@
+//DESC:Demonstrates bytecode created by the assert keyword
+public class Assert
+{
+    public static void main(String[] args)
+    {
+        int a = 42;
+
+        assert(a > 0);
+    }
+}
